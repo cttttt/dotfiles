b0eb61cb323d6cf51c4bffc336cc3b184db249ee
Chris Taylor <ctaylorr@gmail.com>
2019-03-23 18:43:03 -0400

    Improvements
    - Reduce the width of the prompt
    - Make cd-ing more convenient

---
+++ dot/bashrc.d/cd.bash
@@ -0,0 +1,14 @@
+cd () {
+  local dir=$1
+  local prefixes=( '.' "$HOME/src/github.com/cttttt" "$HOME/src/github.com" "$HOME/src/$dir" )
+
+  for prefix in "${prefixes[@]}"; do
+    local project_dir="$prefix/$dir"
+    if [[ -d "$project_dir" ]]; then
+      command cd "$project_dir"
+      return
+    fi
+  done
+
+  command cd "$@"
+}
---
+++ dot/bashrc.d/ps1.bash
@@ -0,0 +1,1 @@
+export PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\n\$ '
