fcc642103d642e65ef60130f1a1600c816b34e8d
Chris <ctaylorr@gmail.com>
2019-03-23 20:10:27 -0400

    Add kube-context and current branch to prompt

--- dot/bashrc.d/ps1.bash
+++ dot/bashrc.d/ps1.bash
@@ -1,1 +1,30 @@
-export PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\n\$ '
+kube_context () {
+  local context
+  context=$(kubectl config current-context 2>/dev/null)
+  local docker_glyph=''
+
+  if [[ $? == 0 ]]; then
+    context="[${docker_glyph} ${context}]"
+  else
+    context=""
+  fi
+
+  echo "$context"
+}
+
+git_branch () {
+  local branch
+  local git_nerd_glyph=''
+
+  branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
+  
+  if [[ $? == 0 ]]; then
+    branch="[${git_nerd_glyph} ${branch}]"
+  else
+    branch=""
+  fi
+
+  echo "$branch"
+}
+
+export PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w $(kube_context) $(git_branch)\n\$ '
