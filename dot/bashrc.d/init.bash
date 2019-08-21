3561de3feb23d87601d4c80b6807771c1489e0ae
Chris Taylor <ctaylorr@gmail.com>
2019-08-20 20:54:08 -0400

    Make it easier to create new repos

---
+++ dot/bashrc.d/init.bash
@@ -0,0 +1,19 @@
+init () {
+  local repo_base_dir="$HOME/src/github.com/cttttt"
+
+  if [[ $# != 1 ]]; then
+    cat >&2 <<USAGE
+usage: init SLUG
+
+Initialize a Git repo under $repo_base_dir/.git, creating it if it doesn't
+already exist.  To change to it, run "cd SLUG".
+
+To run the real init, run "command init".
+USAGE
+    return 1
+  fi
+
+  slug="$1"
+
+  git init "$repo_base_dir/$slug" 
+}
