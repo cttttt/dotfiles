bed6de2a0fb5baf0e6a80175239747b8e020bc4b
Chris Taylor <ctaylorr@gmail.com>
2023-06-09 01:30:05 -0400

    use chruby on linux as well

--- dot/bashrc.d/chruby.bash
+++ dot/bashrc.d/chruby.bash
@@ -2,6 +2,8 @@ load_chruby () {
   local chruby_scripts=(
     /opt/homebrew/opt/chruby/share/chruby/chruby.sh
     /opt/homebrew/opt/chruby/share/chruby/auto.sh
+    /usr/local/share/chruby/auto.sh
+    /usr/local/share/chruby/chruby.sh
   )
 
   for script in "${chruby_scripts[@]}"; do 
