7ca4c7159bac23eb8c66a48a28406614c2fb231f
Chris Taylor <cttttt@users.noreply.github.com>
2023-08-30 13:42:25 -0400

    extend cd so ... changes to the project directory
    Lately, I've been working in large repositories with very deep
    nesting.
    
    After changing to a deeply nested directory wihtin a project
    it's annoying changing back to the project root directory. This adjusts
    my `cd` function to change to the project root when `...` is the dir.
    
    Note that because I'm using `cd` to move up the directory tree,
    care is taken to ensure the search is run in a subshell.

--- dot/bashrc.d/cd.bash
+++ dot/bashrc.d/cd.bash
@@ -1,7 +1,37 @@
+find_project_dir_helper () {
+  if [[ -d '.git' ]]; then
+    pwd
+    return
+  fi
+
+  if [[ $PWD == '/' ]]; then
+    return
+  fi
+
+  command cd ..
+  find_project_dir
+}
+
+find_project_dir () {
+  ( find_project_dir_helper; )
+}
+
 cd () {
+  local project_dir
+
   if [[ $# == 0 || $1 =~ ^/+ ]]; then
     command cd "$@"
     return "$?"
+  fi
+
+  if [[ "$#" == 1 && "$1" == '...' ]]; then
+    project_dir=$(find_project_dir)
+
+    if [[ $project_dir ]]; then
+      cd "$project_dir"
+    fi
+
+    return
   fi
 
   local dir=$1
