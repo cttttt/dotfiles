31278dfd1534fa902e1c4b31e784fe8c7d547ba7
Chris Taylor <cttttt@users.noreply.github.com>
2023-09-07 22:02:57 -0400

    source fzf bash extension scripts on Linux (from packer)

--- dot/bashrc.d/setup-fzf.bash
+++ dot/bashrc.d/setup-fzf.bash
@@ -9,8 +9,13 @@ fi
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview='test -d {} || bat --style=numbers --color=always {}'"
 
-fzf_keybindings_script=~/.config/nvim/plugged/fzf/shell/key-bindings.bash
+fzf_shell_setup_scripts=(
+  ~/.config/nvim/plugged/fzf/shell/{key-bindings.bash,completion.bash}
+  ~/.local/share/nvim/site/pack/packer/start/fzf/shell/{key-bindings.bash,completion.bash}
+)
 
-if [[ -f $fzf_keybindings_script ]]; then
-  source "$fzf_keybindings_script"
-fi
+for fzf_shell_setup_script in "${fzf_shell_setup_scripts[@]}"; do
+  if [[ -f $fzf_shell_setup_script ]]; then
+    source "$fzf_shell_setup_script"
+  fi
+done
