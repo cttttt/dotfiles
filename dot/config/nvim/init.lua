cf63ae1a53092ccf2a0f6e6c5ca6b6720731b5d4
Chris Taylor <cttttt@users.noreply.github.com>
2023-09-23 19:29:04 -0400

    allow multiple files to be open at once

--- dot/config/nvim/init.lua
+++ dot/config/nvim/init.lua
@@ -78,6 +78,21 @@ require("lazy").setup({
       local telescope = require('telescope')
       local actions = require('telescope.actions')
 
+      local select_one_or_multi = function(prompt_bufnr)
+        local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+        local multi = picker:get_multi_selection()
+        if not vim.tbl_isempty(multi) then
+          actions.close(prompt_bufnr)
+          for _, j in pairs(multi) do
+            if j.path ~= nil then
+              vim.cmd(string.format("%s %s", "edit", j.path))
+            end
+          end
+        else
+          actions.select_default(prompt_bufnr)
+        end
+      end
+
       telescope.setup({
         defaults = {
           mappings = {
@@ -86,6 +101,7 @@ require("lazy").setup({
               ['<C-k>'] = 'move_selection_previous',
               ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_worse,
               ['<Tab>'] = actions.toggle_selection + actions.move_selection_better,
+              ["<CR>"] = select_one_or_multi,
             },
           },
         },
@@ -199,3 +215,4 @@ end, { bang = true, nargs = 1 })
 vim.api.nvim_create_user_command('Ag', function (opts)
   require('telescope.builtin').live_grep()
 end, { nargs = 0 })
+
