e2228474bd7bcc0509af5a8aff2ec994c8d05afd
Chris Taylor <cttttt@users.noreply.github.com>
2023-09-23 11:56:17 -0400

    mosh doesn't work well with basically all unicode special chars
    the bufferbar causes the terminal to render incorrectly when using mosh.
    eternal terminal seems to work better.  this will encourage me to use et instead
    of mosh.

---
+++ dot/bashrc.d/try-et-instead-of-mosh.bash
@@ -0,0 +1,16 @@
+mosh () {
+  cat <<DONE
+Consider using Eternal Terminal instead of mosh.
+
+To install on Ubuntu, use:
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:jgmath2000/et
+sudo apt-get update
+sudo apt-get install et
+
+To install on OSX, use:
+
+brew install MisterTea/et/et
+DONE
+}
