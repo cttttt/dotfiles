abacce8283edf274302dc767c40b41fbed1a9515
Chris Taylor <cttttt@users.noreply.github.com>
2023-09-24 11:04:26 -0400

    typo

--- dot/config/nvim/init.lua
+++ dot/config/nvim/init.lua
@@ -225,7 +225,7 @@ end, { bang = true })
 table.unpack = table.unpack or unpack
 
 -- for some reason, rhubarb uses netrw's Browse to open
--- things, like urls. vim-git requires me to disable netrw.
+-- things, like urls. vim-tree requires me to disable netrw.
 vim.api.nvim_create_user_command('Browse', function(opts)
   vim.system({'open', table.unpack(opts.fargs)}, {}, nil)
 end, { bang = true, nargs = 1 })
