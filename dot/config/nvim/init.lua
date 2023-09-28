6f0ad8b8df717c577fdf68c693f9963986080fe7
Chris Taylor <cttttt@users.noreply.github.com>
2023-09-27 21:48:22 -0400

    format

--- dot/config/nvim/init.lua
+++ dot/config/nvim/init.lua
@@ -28,7 +28,11 @@ vim.keymap.set('n', '<leader>n', function ()
 end, {})
 
 vim.keymap.set('n', '<leader>N', function ()
-  require("nvim-tree.api").tree.find_file( { open = true, focus = true, update_root = true } )
+  require("nvim-tree.api").tree.find_file({
+    open = true,
+    focus = true,
+    update_root = true,
+  })
 end, {})
 
 vim.keymap.set('v', '<C-c>', '"+y', {})
