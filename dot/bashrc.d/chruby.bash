f8bbd7779a207cd9c22db0d572e2190d558d62a3
Chris Taylor <cttttt@users.noreply.github.com>
2023-09-30 11:40:54 -0400

    load chruby before loading autochruby

--- dot/bashrc.d/chruby.bash
+++ dot/bashrc.d/chruby.bash
@@ -2,8 +2,8 @@ load_chruby () {
   local chruby_scripts=(
     /opt/homebrew/opt/chruby/share/chruby/chruby.sh
     /opt/homebrew/opt/chruby/share/chruby/auto.sh
-    /usr/local/share/chruby/auto.sh
     /usr/local/share/chruby/chruby.sh
+    /usr/local/share/chruby/auto.sh
   )
 
   for script in "${chruby_scripts[@]}"; do 
