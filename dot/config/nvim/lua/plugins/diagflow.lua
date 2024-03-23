0ccc2c6ca15f79af7d8b5d876c03f7d12d8dd40d
Chris Taylor <cttttt@users.noreply.github.com>
2024-03-23 09:44:58 -0400

    configure diagflow a bit

--- dot/config/nvim/lua/plugins/diagflow.lua
+++ dot/config/nvim/lua/plugins/diagflow.lua
@@ -1,7 +1,12 @@
 return {
   'dgagn/diagflow.nvim',
+  event = 'LspAttach',
   config = true,
   opts = {
     scope = 'line',
-  }
+    show_sign = true,
+    enable = function()
+      return vim.bo.filetype ~= "lazy" and vim.bo.filetype ~= "neo-tree"
+    end,
+  },
 }
