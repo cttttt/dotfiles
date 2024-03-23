df9b7cd5127d6ec79bc1ae5c8e4bfc54864aade8
Chris Taylor <cttttt@users.noreply.github.com>
2024-03-23 10:30:15 -0400

    finally get around to disabling mini-completion for neotree popups

--- dot/config/nvim/lua/plugins/mini.lua
+++ dot/config/nvim/lua/plugins/mini.lua
@@ -7,5 +7,14 @@ return {
         source_func = 'omnifunc',
       }
     })
+
+    -- see :help mini.nvim-disabling-recipes the mini-completion help
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'neo-tree*',
+      callback = function ()
+        vim.b.minicompletion_disable = true
+      end
+    })
+
   end,
 }
