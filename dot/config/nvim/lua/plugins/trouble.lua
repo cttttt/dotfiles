8d81216674295ff6c42e8ed1b281ad41deb43c00
Chris Taylor <cttttt@users.noreply.github.com>
2024-05-14 22:29:50 -0400

    use trouble beta

--- dot/config/nvim/lua/plugins/trouble.lua
+++ dot/config/nvim/lua/plugins/trouble.lua
@@ -1,6 +1,15 @@
 return {
   'folke/trouble.nvim',
+  branch = 'dev',
   dependencies = {
     'nvim-tree/nvim-web-devicons'
-  }
+  },
+  config = true,
+  keys = {
+    {
+      "<leader>ct",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+  },
 }
