800a2a1cf8427081dd2b02cb78a45687611a4e51
Chris Taylor <cttttt@users.noreply.github.com>
2024-06-24 18:58:02 -0400

    automatically install language servers

--- dot/config/nvim/lua/plugins/mason-lspconfig.lua
+++ dot/config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,3 +1,16 @@
 return {
   'williamboman/mason-lspconfig.nvim',
+   opts = {
+     ensure_installed = {
+      "gopls",
+      "rust_analyzer",
+      "solargraph",
+      "basedpyright",
+      "tsserver",
+      "terraformls",
+      "lua_ls",
+      "helm_ls",
+      "yamlls",
+     }
+   }
 }
