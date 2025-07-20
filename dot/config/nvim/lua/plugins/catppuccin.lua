130af2f2c4777c12b9751f7054590ee457589ed0
Chris Taylor <cttttt@users.noreply.github.com>
2025-07-19 20:44:17 -0400

    use higher contrast col/line highlight

--- dot/config/nvim/lua/plugins/catppuccin.lua
+++ dot/config/nvim/lua/plugins/catppuccin.lua
@@ -4,5 +4,7 @@ return {
   priority = 1000,
   config = function ()
     vim.cmd.colorscheme('catppuccin')
+    vim.api.nvim_set_hl(0, 'CursorColumn', { bg = "#505050" })
+    vim.api.nvim_set_hl(0, 'CursorLine', { bg = "#505050" })
   end
 }
