3cef646a6534f604747f96e9d4b25e8825747a52
Chris Taylor <cttttt@users.noreply.github.com>
2026-05-22 21:46:27 -0400

    fix shift+enter in kitty -> nvim -> termbuf -> tmux

--- dot/config/nvim/lua/plugins/sidekick.lua
+++ dot/config/nvim/lua/plugins/sidekick.lua
@@ -4,6 +4,20 @@ return {
     -- add any options here
     picker = "telescope",
     cli = {
+      win = {
+        keys = {
+          shift_cr = { "<S-CR>", function(self)
+            if self:is_running() then
+              vim.api.nvim_chan_send(self.job, "\x1b[13;2u")
+            end
+          end, mode = "t", desc = "CSI-u Shift+Enter" },
+          alt_cr = { "<A-CR>", function(self)
+            if self:is_running() then
+              vim.api.nvim_chan_send(self.job, "\x1b[13;3u")
+            end
+          end, mode = "t", desc = "CSI-u Alt+Enter" },
+        },
+      },
       mux = {
         backend = "tmux",
         enabled = true,
