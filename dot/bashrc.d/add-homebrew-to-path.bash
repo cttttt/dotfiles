6ec26d05ebf5dd7e783a243bcd2a34ed99483969
Chris Taylor <cttttt@users.noreply.github.com>
2026-07-18 21:27:22 -0400

    fix: typeset -U isn't a bash thing

--- dot/bashrc.d/add-homebrew-to-path.bash
+++ dot/bashrc.d/add-homebrew-to-path.bash
@@ -1,2 +1,7 @@
-typeset -U path
-path=($path /opt/homebrew/sbin /opt/homebrew/bin)
+# Use path array in zsh, but standard PATH export in bash
+if [[ -n $ZSH_VERSION ]]; then
+  typeset -U path
+  path=($path /opt/homebrew/sbin /opt/homebrew/bin)
+else
+  export PATH="/opt/homebrew/sbin:/opt/homebrew/bin:$PATH"
+fi
